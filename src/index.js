f524b58a36a924e7457b585411d29570533d17ec
jeangathoni8 <jeangathoni39@gmail.com>
2024-04-03 10:18:46 +0300

    task lister lab

--- src/index.js
+++ src/index.js
@@ -26,9 +26,14 @@ function deleteTask(e){
 }
 
 // Adds a click listener to each task in the list that will remove it when clicked.
+const deleteButton = document.createElement('button');
+deleteButton.textContent = 'X';
+deleteButton.className = "delete";
 const tasks = document.getElementsByTagName('li');
 for (let i = 0; i < tasks.length; i++) {
   const task = tasks[i];
+  deleteButton.addEventListener('click', deleteTask);
+  task.appendChild(deleteButton.cloneNode());
   task.addEventListener('click', deleteTask);
 }
 

b1c50c30db902ba1e64a14d423db1db2af700478
jeangathoni8 <jeangathoni39@gmail.com>
2024-04-03 09:26:45 +0300

    lab done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  
+  if (form) {
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+      const task = document.getElementById('new-task-description');
+      const taskList = document.getElementById('tasks');
+      const newTask = document.createElement('li');
+      newTask.innerText = task.value;
+      taskList.appendChild(newTask);
+      task.value = '';
+    });
+  } else {
+    console.log('form not found');
+  }
 });
+
+function deleteTask(e){
+  e.target.parentNode.remove();
+}
+
+// Adds a click listener to each task in the list that will remove it when clicked.
+const tasks = document.getElementsByTagName('li');
+for (let i = 0; i < tasks.length; i++) {
+  const task = tasks[i];
+  task.addEventListener('click', deleteTask);
+}
+
